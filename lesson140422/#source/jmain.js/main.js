fa5ab4ec76fab5096d8033d2cc02ba12c1de5d5c
AleksiKlimov <101139243+AleksiKlimov@users.noreply.github.com>
2022-07-17 18:52:29 +0300

    Add files via upload

---
+++ lesson140422/#source/jmain.js/main.js
@@ -0,0 +1,9 @@
+//@prepros-append header.js;
+
+$(document).ready(function () {
+	$('.parallax__list>li').addClass('layer')
+	$('.parallax__list').parallax()
+
+	$('.wrapper').addClass('active')
+});
+//Object/////////////////////////////////////////////////////////////////////////////
